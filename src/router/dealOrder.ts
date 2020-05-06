af9b6ed4c5ff832e821006c6cca238911adeaaa0
Xaber <xaber@foxmail.com>
2020-05-06 14:52:26 +0800

    chore: remove notify order status

--- src/router/dealOrder.ts
+++ src/router/dealOrder.ts
@@ -2,20 +2,7 @@ import * as Sentry from '@sentry/node'
 import tracker from '../utils/tracker'
 import { dealOrder as dealOrderToMM } from '../request/marketMaker'
 import { DealOrder } from '../types'
-import { getTimestamp } from '../utils/timestamp'
 import { removeQuoteIdPrefix } from '../utils/quoteId'
-
-let notifiedOrders = []
-
-const isNotifiedOrder = (order) => {
-  return !!notifiedOrders.find(o => o.quoteId === order.quoteId)
-}
-
-const handleNotifiedOrder = (order) => {
-  // 只保留一天的成交已通知订单
-  notifiedOrders = notifiedOrders.filter(o => o.timestamp >= getTimestamp() - 24 * 60 * 60)
-  notifiedOrders.push(order)
-}
 
 export const dealOrder = async (ctx) => {
   const { makerToken, takerToken, makerTokenAmount, takerTokenAmount, quoteId, timestamp } = ctx.request.body as DealOrder
@@ -28,20 +15,13 @@ export const dealOrder = async (ctx) => {
     extra: order,
   })
 
-  if (!isNotifiedOrder(order)) {
-    try {
-      const res = await dealOrderToMM(order)
-      if (!res.result) {
-        reqToMMErrMsg = 'MM deal API not response result true'
-      }
-    } catch (e) {
-      reqToMMErrMsg = e.message
+  try {
+    const res = await dealOrderToMM(order)
+    if (!res.result) {
+      reqToMMErrMsg = 'MM deal API not response result true'
     }
-
-    handleNotifiedOrder(order)
-
-  } else {
-    tracker.captureMessage('deal order alread notified', Sentry.Severity.Info)
+  } catch (e) {
+    reqToMMErrMsg = e.message
   }
 
   ctx.body = {
