f06b64cb04a134fbef11e24be61152f50342f796
Kai <kai@thekaiway.com>
2020-06-15 11:38:23 +0800

    feat: use cluster to run server

--- app/start.js
+++ app/start.js
@@ -1,6 +1,21 @@
-require('dotenv').config()
-
 const mmConf = require('./mmConfig')
 const mmsk = require('../lib')
 
-mmsk.startMMSK(mmConf)
+const cluster = require('cluster')
+const numCPUs = require('os').cpus().length
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`)
+  })
+} else {
+  mmsk.startMMSK(mmConf)
+  console.log(`Worker ${process.pid} started`)
+}
--- benchmark/Start.js
+++ benchmark/Start.js
@@ -1,6 +1,3 @@
-//import { startMMSK } from '../src'
-//import { config as loadEnvConfig } from 'dotenv'
-
 const startMMSK = require("../lib").startMMSK
 const loadEnvConfig = require("dotenv").config
 
@@ -20,4 +17,22 @@ async function main() {
   })
 }
 
-main().catch(console.error)
+const cluster = require('cluster')
+const numCPUs = require('os').cpus().length
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`)
+  })
+} else {
+  main().catch(console.error)
+
+  console.log(`Worker ${process.pid} started`)
+}
