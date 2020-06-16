c75cca89ceda00e4125069d328434181094b537b
Xaber <xaber@foxmail.com>
2020-06-16 18:30:17 +0800

    fix: query.feeFactor to query.feefactor

--- src/router/newOrder.ts
+++ src/router/newOrder.ts
@@ -4,11 +4,12 @@ import { PriceApiResult } from '../request/marketMaker/interface'
 import { getFormatedSignedOrder } from '../utils/order'
 import { getSupportedTokens, translateQueryData } from '../utils/token'
 import { updaterStack } from '../utils/intervalUpdater'
+import { QueryInterface } from './interface'
 import { checkParams } from '../validations'
 import { transferPriceResultToRateBody } from '../utils/rate'
 
 export const newOrder = async (ctx) => {
-  const { query } = ctx
+  const query: QueryInterface = ctx.query
   const updatedQueryData = translateQueryData(query)
   const checkResult = checkParams(updatedQueryData, true)
   let rateBody = {} as any
@@ -20,7 +21,7 @@ export const newOrder = async (ctx) => {
 
   try {
     const { side } = updatedQueryData
-    const priceResult = await getPrice(updatedQueryData)
+    const priceResult = await getPrice(updatedQueryData as any)
     rateBody = transferPriceResultToRateBody(priceResult as PriceApiResult, side) as any
 
   } catch (e) {
@@ -37,6 +38,8 @@ export const newOrder = async (ctx) => {
 
   } else {
     const { rate, minAmount, maxAmount, quoteId } = rateBody
+    // 注意：query 上，后端传递的是 feefactor，而不是 feeFactor
+    // 但是，Token Config 返回的配置是 feeFactor
     const { userAddr, feefactor } = updatedQueryData
     const config = updaterStack.markerMakerConfigUpdater.cacheResult
     const tokenConfigs = updaterStack.tokenConfigsFromImtokenUpdater.cacheResult
--- src/utils/token.ts
+++ src/utils/token.ts
@@ -2,6 +2,7 @@ import * as _ from 'lodash'
 import { updaterStack } from '../utils/intervalUpdater'
 import { SupportedToken } from '../types'
 import { roundAmount } from './format'
+import { QueryInterface } from '../router/interface'
 
 const helper = (stack, token1, token2) => {
   if (stack[token1] && stack[token1].indexOf(token2) === -1) {
@@ -58,12 +59,11 @@ export const getTokenBySymbol = (tokens, symbol) => {
 }
 
 // 处理接口大小写情况，转换为系统设定格式，以及 side BUY 情况的数量调整
-export const translateQueryData = (query) => {
+export const translateQueryData = (query: QueryInterface): QueryInterface => {
   const tokens = getSupportedTokens()
   let updatedBase = null
   let updatedQuote = null
   let updatedAmount = null
-  let updatedFeeFactor = null
 
   if (_.isString(query.base)) {
     const found = tokens.find(t => t.symbol.toUpperCase() === query.base.toUpperCase())
@@ -76,13 +76,15 @@ export const translateQueryData = (query) => {
     if (side === 'BUY') {
       const config = updaterStack.markerMakerConfigUpdater.cacheResult
       const tokenConfigs = updaterStack.tokenConfigsFromImtokenUpdater.cacheResult
-      const queryFeeFactor = query.feeFactor
+      // 注意：query 上，后端传递的是 feefactor，而不是 feeFactor
+      // 但是，Token Config 返回的配置是 feeFactor
+      const queryFeeFactor = query.feefactor
       // 用户 BUY base, 手续费就是 base 的 Token，即 order的 makerToken —— 对应做市商转出的币，用户收到的币
       const foundTokenConfig = tokenConfigs.find(t => t.symbol === query.base)
-      updatedFeeFactor = !_.isUndefined(queryFeeFactor) && !_.isNaN(+queryFeeFactor) && +queryFeeFactor >= 0 ? +queryFeeFactor : (
+      const feeFactor = !_.isUndefined(queryFeeFactor) && !_.isNaN(+queryFeeFactor) && +queryFeeFactor >= 0 ? +queryFeeFactor : (
         foundTokenConfig && foundTokenConfig.feeFactor ? foundTokenConfig.feeFactor : (config.feeFactor ? config.feeFactor : 0)
       )
-      updatedAmount = amount ? roundAmount(amount / (1 - updatedFeeFactor / 10000), 4) : amount
+      updatedAmount = amount ? roundAmount(+amount / (1 - feeFactor / 10000), 4) : amount
     }
   }
   if (_.isString(query.quote)) {
@@ -98,7 +100,6 @@ export const translateQueryData = (query) => {
     base: updatedBase,
     quote: updatedQuote,
     amount: updatedAmount,
-    feeFactor: updatedFeeFactor,
   }).forEach(([key, value]) => {
     if (value) {
       Object.assign(result, {
