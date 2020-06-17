c96fffe3093352dc649bed69db675719fdb94d2e
Kai <kai@thekaiway.com>
2020-06-18 00:33:28 +0800

    refactor: break the long translateQueryData function to small pieces

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,5 +1,4 @@
-import * as _ from 'lodash'
-import { updaterStack } from '../utils/intervalUpdater'
+import { updaterStack } from './intervalUpdater'
 import { roundAmount } from './format'
 import { QueryInterface } from '../router/interface'
 import { getSupportedTokens } from './token'
@@ -11,55 +10,57 @@ export const leftPadWith0 = (str, len) => {
   return '0'.repeat(len) + str
 }
 
-// 处理接口大小写情况，转换为系统设定格式，以及 side BUY 情况的数量调整
-export const translateQueryData = (query: QueryInterface): QueryInterface => {
+function applyFeeToAmount(amount: number, feeFactor: number) {
+  return amount ? roundAmount(+amount / (1 - feeFactor / 10000), 4) : amount
+}
+
+function calculateFeeFactor(baseSymbol: string, factor: number | null): number {
+  const tokenConfigs = updaterStack.tokenConfigsFromImtokenUpdater.cacheResult
+  // 用户 BUY base, 手续费就是 base 的 Token，即 order的 makerToken —— 对应做市商转出的币，用户收到的币
+  // 但是，Token Config 返回的配置是 feeFactor
+  const foundTokenConfig = tokenConfigs.find(t => t.symbol.toUpperCase() === baseSymbol)
+
+  const config = updaterStack.markerMakerConfigUpdater.cacheResult
+  let result = config.feeFactor ? config.feeFactor : 0
+
+  const queryFeeFactor = Number(factor)
+  if (!isNaN(queryFeeFactor) && queryFeeFactor >= 0) {
+    result = queryFeeFactor
+  } else if (foundTokenConfig && foundTokenConfig.feeFactor) {
+    result = foundTokenConfig.feeFactor
+  }
+  return result
+}
+
+function ensureCorrectSymbolCase(query: QueryInterface): QueryInterface {
   const tokens = getSupportedTokens()
-  let updatedBase = null
-  let updatedQuote = null
-  let updatedAmount = null
-
-  if (_.isString(query.base)) {
-    const found = tokens.find(t => t.symbol.toUpperCase() === query.base.toUpperCase())
-    if (found) {
-      updatedBase = found.symbol
-    }
-
-    const { amount, side } = query
-
-    if (side === 'BUY') {
-      const config = updaterStack.markerMakerConfigUpdater.cacheResult
-      const tokenConfigs = updaterStack.tokenConfigsFromImtokenUpdater.cacheResult
-      // 注意：query 上，后端传递的是 feefactor，而不是 feeFactor
-      // 但是，Token Config 返回的配置是 feeFactor
-      const queryFeeFactor = query.feefactor
-      // 用户 BUY base, 手续费就是 base 的 Token，即 order的 makerToken —— 对应做市商转出的币，用户收到的币
-      const foundTokenConfig = tokenConfigs.find(t => t.symbol.toUpperCase() === query.base.toUpperCase())
-      const feeFactor = !_.isUndefined(queryFeeFactor) && !_.isNaN(+queryFeeFactor) && +queryFeeFactor >= 0 ? +queryFeeFactor : (
-        foundTokenConfig && foundTokenConfig.feeFactor ? foundTokenConfig.feeFactor : (config.feeFactor ? config.feeFactor : 0)
-      )
-      updatedAmount = amount ? roundAmount(+amount / (1 - feeFactor / 10000), 4) : amount
-    }
-  }
-  if (_.isString(query.quote)) {
-    const found = tokens.find(t => t.symbol.toUpperCase() === query.quote.toUpperCase())
-    if (found) {
-      updatedQuote = found.symbol
-    }
-  }
-
   const result = { ...query }
 
-  Object.entries({
-    base: updatedBase,
-    quote: updatedQuote,
-    amount: updatedAmount,
-  }).forEach(([key, value]) => {
-    if (value) {
-      Object.assign(result, {
-        [key]: value,
-      })
+  if (typeof query.base === 'string') {
+    const found = tokens.find(t => t.symbol.toUpperCase() === query.base.toUpperCase())
+    if (found) {
+      result.base = found.symbol
     }
-  })
-
+  }
+  if (typeof query.quote === 'string') {
+    const found = tokens.find(t => t.symbol.toUpperCase() === query.quote.toUpperCase())
+    if (found) {
+      result.quote = found.symbol
+    }
+  }
   return result
+}
+
+function processBuyAmount(query: QueryInterface): QueryInterface {
+  const result = { ...query }
+  if (typeof query.base === 'string' && query.side === 'BUY') {
+    // 注意：query 上，后端传递的是 feeFactor，而不是 feeFactor
+    result.amount = applyFeeToAmount(calculateFeeFactor(query.base.toUpperCase(), query.feefactor), query.amount)
+  }
+  return result
+}
+
+// 处理接口大小写情况，转换为系统设定格式，以及 side BUY 情况的数量调整
+export const translateQueryData = (query: QueryInterface): QueryInterface => {
+  return processBuyAmount(ensureCorrectSymbolCase(query))
 }
