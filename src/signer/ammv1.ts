17487352583ca474b8b0a6a98ddc1b98b261c47b
Kai <kai@thekaiway.com>
2020-12-09 12:56:33 +0800

    feat: fix the eth address for amm order

--- src/signer/ammv1.ts
+++ src/signer/ammv1.ts
@@ -1,22 +1,31 @@
 // sign order from custom quoter no need to put it in MMSK
-import { generatePseudoRandomSalt } from '0x-v2-order-utils'
+import { assetDataUtils, generatePseudoRandomSalt } from '0x-v2-order-utils'
 import * as cryptoRandomString from 'crypto-random-string'
 import { orderBNToString } from '../utils/format'
 import { getOrderAndFeeFactor } from './pmmv4'
+import { NULL_ADDRESS } from '../constants'
 
 export const buildSignedOrder = (params) => {
+  const { config } = params
   const { order, feeFactor } = getOrderAndFeeFactor(params)
-  console.log(order)
+  // = Rewrite order fields
+  // 1. change maker address to LP pool address
   order.makerAddress = params.makerAddress
-  const o = {
-    ...order,
-    salt: generatePseudoRandomSalt(),
+  // 2. convert weth to eth
+  const makerTokenAddress = assetDataUtils.decodeERC20AssetData(order.makerAssetData).tokenAddress
+  if (makerTokenAddress.toLowerCase() === config.wethContractAddress.toLowerCase()) {
+    order.makerAssetData = assetDataUtils.encodeERC20AssetData(NULL_ADDRESS)
   }
-  const makerWalletSignature = cryptoRandomString({ length: 40 })
+  const takerTokenAddress = assetDataUtils.decodeERC20AssetData(order.takerAssetData).tokenAddress
+  if (takerTokenAddress.toLowerCase() === config.wethContractAddress.toLowerCase()) {
+    order.takerAssetData = assetDataUtils.encodeERC20AssetData(NULL_ADDRESS)
+  }
+  // NOTE: for AMM order we don't do signing here
   const signedOrder = {
-    ...o,
+    ...order,
     feeFactor,
-    makerWalletSignature,
+    salt: generatePseudoRandomSalt(),
+    makerWalletSignature: cryptoRandomString({ length: 40 }),
   }
   return orderBNToString(signedOrder)
 }
