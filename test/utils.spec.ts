f07cf7e6b57f7c05e3cad3d83696f065630c00d2
Miya Chen <miyatlchen@gmail.com>
2021-07-06 23:04:31 +0800

    fix tests

--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -12,7 +12,37 @@ const supportTokens = [
     precision: 4,
     minTradeAmount: 1e-7,
     maxTradeAmount: 10,
-    opposites: ['DAI', 'imBTC'],
+    opposites: [
+      {
+        symbol: 'DAI',
+        logo: 'https://cdn.example.com/dai.png',
+        contractAddress: '0x5c964665b6379527b625be996020d861f27aa31d',
+        decimal: 18,
+        precision: 4,
+        minTradeAmount: 0.001,
+        maxTradeAmount: 10000,
+      },
+      {
+        symbol: 'imBTC',
+        logo: 'https://cdn.example.com/dai.png',
+        contractAddress: '0x5c964665b6379527b625be996020d861f27aa31d',
+        decimal: 18,
+        precision: 4,
+        minTradeAmount: 0.001,
+        maxTradeAmount: 10000,
+        opposites: [
+          {
+            symbol: 'ETH',
+            logo: 'https://cdn.example.com/eth.png',
+            contractAddress: '0x0000000000000000000000000000000000000000',
+            decimal: 18,
+            precision: 4,
+            minTradeAmount: 1e-7,
+            maxTradeAmount: 10,
+          },
+        ],
+      },
+    ],
   },
   {
     symbol: 'DAI',
@@ -22,7 +52,17 @@ const supportTokens = [
     precision: 4,
     minTradeAmount: 0.001,
     maxTradeAmount: 10000,
-    opposites: ['ETH'],
+    opposites: [
+      {
+        symbol: 'ETH',
+        logo: 'https://cdn.example.com/eth.png',
+        contractAddress: '0x0000000000000000000000000000000000000000',
+        decimal: 18,
+        precision: 4,
+        minTradeAmount: 1e-7,
+        maxTradeAmount: 10,
+      },
+    ],
   },
   {
     symbol: 'imBTC',
@@ -32,7 +72,17 @@ const supportTokens = [
     precision: 4,
     minTradeAmount: 0.001,
     maxTradeAmount: 10000,
-    opposites: ['ETH'],
+    opposites: [
+      {
+        symbol: 'ETH',
+        logo: 'https://cdn.example.com/eth.png',
+        contractAddress: '0x0000000000000000000000000000000000000000',
+        decimal: 18,
+        precision: 4,
+        minTradeAmount: 1e-7,
+        maxTradeAmount: 10,
+      },
+    ],
   },
 ]
 
@@ -41,7 +91,9 @@ describe('translateBaseQuote()', function () {
     let pair: QueryInterface = { base: 'DAI', quote: 'ETH', side: 'BUY' }
     expect(ensureCorrectSymbolCase(pair, supportTokens)).to.eql({
       base: 'DAI',
+      baseAddress: '0x5c964665b6379527b625be996020d861f27aa31d',
       quote: 'ETH',
+      quoteAddress: '0x0000000000000000000000000000000000000000',
       side: 'BUY',
     })
   })
@@ -50,7 +102,9 @@ describe('translateBaseQuote()', function () {
     let pair: QueryInterface = { base: 'DAI', quote: 'Eth', side: 'BUY' }
     expect(ensureCorrectSymbolCase(pair, supportTokens)).to.eql({
       base: 'DAI',
+      baseAddress: '0x5c964665b6379527b625be996020d861f27aa31d',
       quote: 'ETH',
+      quoteAddress: '0x0000000000000000000000000000000000000000',
       side: 'BUY',
     })
   })
@@ -59,7 +113,9 @@ describe('translateBaseQuote()', function () {
     let pair: QueryInterface = { base: 'IMBTC', quote: 'ETH', side: 'BUY' }
     expect(ensureCorrectSymbolCase(pair, supportTokens)).to.eql({
       base: 'imBTC',
+      baseAddress: '0x5c964665b6379527b625be996020d861f27aa31d',
       quote: 'ETH',
+      quoteAddress: '0x0000000000000000000000000000000000000000',
       side: 'BUY',
     })
   })
