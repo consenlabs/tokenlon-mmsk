d15bbb37689b58349c6f2f77fe33556cc19c21e7
Charles Jhong <charlesjhongc@gmail.com>
2023-09-13 10:33:48 +0800

    update prettier and format contracts

--- contracts/SafeToken.sol
+++ contracts/SafeToken.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.0;
 
-import "./IERC20NonStandard.sol";
+import './IERC20NonStandard.sol';
 
 contract SafeToken {
     function doApprove(address token, address spender, uint256 amount) internal {
@@ -10,22 +10,22 @@ contract SafeToken {
 
         assembly {
             switch returndatasize()
-                case 0 {                      // This is a non-standard ERC-20
-                    result := not(0)          // set result to true
-                }
-                case 32 {                     // This is a complaint ERC-20
-                    returndatacopy(0, 0, 32)
-                    result := mload(0)        // Set `result = returndata` of external call
-                }
-                default {                     // This is an excessively non-compliant ERC-20, revert.
-                    revert(0, 0)
-                }
+            case 0 {
+                // This is a non-standard ERC-20
+                result := not(0) // set result to true
+            }
+            case 32 {
+                // This is a complaint ERC-20
+                returndatacopy(0, 0, 32)
+                result := mload(0) // Set `result = returndata` of external call
+            }
+            default {
+                // This is an excessively non-compliant ERC-20, revert.
+                revert(0, 0)
+            }
         }
 
-        require(
-            result,
-            "APPROVE_FAILED"
-        );
+        require(result, 'APPROVE_FAILED');
     }
 
     function doTransferFrom(address token, address from, address to, uint256 amount) internal {
@@ -35,21 +35,21 @@ contract SafeToken {
 
         assembly {
             switch returndatasize()
-                case 0 {                      // This is a non-standard ERC-20
-                    result := not(0)          // set result to true
-                }
-                case 32 {                     // This is a complaint ERC-20
-                    returndatacopy(0, 0, 32)
-                    result := mload(0)        // Set `result = returndata` of external call
-                }
-                default {                     // This is an excessively non-compliant ERC-20, revert.
-                    revert(0, 0)
-                }
+            case 0 {
+                // This is a non-standard ERC-20
+                result := not(0) // set result to true
+            }
+            case 32 {
+                // This is a complaint ERC-20
+                returndatacopy(0, 0, 32)
+                result := mload(0) // Set `result = returndata` of external call
+            }
+            default {
+                // This is an excessively non-compliant ERC-20, revert.
+                revert(0, 0)
+            }
         }
 
-        require(
-            result,
-            "TRANSFER_FROM_FAILED"
-        );
+        require(result, 'TRANSFER_FROM_FAILED');
     }
 }
